db47094fb25d19f72e99fccacf1615d038ae1973
codingwithsurya <surya.subramanian@warriorlife.net>
2023-09-22 17:32:19 -0400

    error strings capatalized

--- cmd/backend/build_training_env/build-training-env-file.go
+++ cmd/backend/build_training_env/build-training-env-file.go
@@ -25,13 +25,13 @@ var buildTrainingEnvCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
         sess, err := session.NewSession(&aws.Config{Region: aws.String(backend.AwsRegion)})
         if err != nil {
-            log.Fatal("Error creating AWS session: ", err)
+            log.Fatal("error session: ", err)
         }
 
         smClient := secretsmanager.New(sess)
         secretValue, err := smClient.GetSecretValue(&secretsmanager.GetSecretValueInput{SecretId: aws.String(secretNameTraining)})
         if err != nil {
-            log.Fatal("Error retrieving secret: ", err)
+            log.Fatal("error retrieving secret: ", err)
         }
 
         path := "./training"
@@ -46,26 +46,26 @@ var buildTrainingEnvCmd = &cobra.Command{
 
 func writeToEnvFile(paramName string, paramValue string, path string) error {
     if err := os.MkdirAll(path, os.ModePerm); err != nil {
-        return fmt.Errorf("Error creating directory: %v", err)
+        return fmt.Errorf("error creating directory: %v", err)
     }
 
     content := strings.ToUpper(paramName) + "=" + paramValue
     f, err := os.OpenFile(path+"/.env", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
-        return fmt.Errorf("Error creating .env file: %v", err)
+        return fmt.Errorf("error creating .env file: %v", err)
     }
     defer f.Close()
 
     fmt.Println("Writing to .env file", path)
     n, err := f.WriteString(content + "\n")
     if err != nil {
-        return fmt.Errorf("Error writing to .env file: %v", err)
+        return fmt.Errorf("error writing to .env file: %v", err)
     }
     fmt.Printf("Wrote %d bytes to .env file\n", n)
 
     err = f.Sync()
     if err != nil {
-        return fmt.Errorf("Error syncing .env file: %v", err)
+        return fmt.Errorf("error syncing .env file: %v", err)
     }
 
     return nil
--- cmd/frontend/build_frontend_env/build-frontend-env-file.go
+++ cmd/frontend/build_frontend_env/build-frontend-env-file.go
@@ -14,6 +14,7 @@ import (
     "github.com/aws/aws-sdk-go/service/secretsmanager"
     "github.com/spf13/cobra"
     "github.com/DSGT-DLP/Deep-Learning-Playground/cli/cmd/frontend" // For frontend/
+
 )
 
 var secretName string // Name of the secret in AWS Secrets Manager.
@@ -25,13 +26,13 @@ var buildFrontendEnvCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
         sess, err := session.NewSession(&aws.Config{Region: aws.String(frontend.AwsRegion)})
         if err != nil {
-            log.Fatal("Error creating AWS session: ", err)
+            log.Fatal("error creating AWS session: ", err)
         }
 
         smClient := secretsmanager.New(sess)
         secretValue, err := smClient.GetSecretValue(&secretsmanager.GetSecretValueInput{SecretId: aws.String(secretName)})
         if err != nil {
-            log.Fatal("Error retrieving secret: ", err)
+            log.Fatal("error retrieving secret: ", err)
         }
 
         path := "./frontend"
@@ -49,26 +50,26 @@ var buildFrontendEnvCmd = &cobra.Command{
 
 func writeToEnvFile(paramName string, paramValue string, path string) error {
     if err := os.MkdirAll(path, os.ModePerm); err != nil {
-        return fmt.Errorf("Error creating directory: %v", err)
+        return fmt.Errorf("error creating directory: %v", err)
     }
 
     content := strings.ToUpper(paramName) + "=" + paramValue
     f, err := os.OpenFile(path+"/.env", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
-        return fmt.Errorf("Error creating .env file: %v", err)
+        return fmt.Errorf("error creating .env file: %v", err)
     }
     defer f.Close()
 
     fmt.Println("Writing to .env file", path)
     n, err := f.WriteString(content + "\n")
     if err != nil {
-        return fmt.Errorf("Error writing to .env file: %v", err)
+        return fmt.Errorf("error writing to .env file: %v", err)
     }
     fmt.Printf("Wrote %d bytes to .env file\n", n)
 
     err = f.Sync()
     if err != nil {
-        return fmt.Errorf("Error syncing .env file: %v", err)
+        return fmt.Errorf("error syncing .env file: %v", err)
     }
 
     return nil
