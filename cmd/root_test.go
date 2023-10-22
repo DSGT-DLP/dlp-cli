8ba664bc150693992d9a0e3f62f4393f0b4fe107
karkir0003 <karkir0003@gmail.com>
2023-10-21 21:46:53 -0400

    root test using testify

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -2,27 +2,41 @@ package cmd
 
 import (
 	"bytes"
-	//"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestRootCmd_Execute(t *testing.T) {
+type testingTAdapter struct {
+    t *testing.T
+}
 
-	// Redirect stdout to a buffer to capture the output
-	var stdout bytes.Buffer
-	RootCmd.SetOut(&stdout)
+/*
+ * Testify framework doesn't natively implement assertion for strings, so we
+ * need to create a custom testing structure that implementes the TestingT
+ * interface
+ */
+func (ta *testingTAdapter) Errorf(format string, args ...interface{}) {
+    ta.t.Errorf(format, args...)
+}
 
-	// Execute the root command
-	err := RootCmd.Execute()
+func TestRootCmd(t *testing.T) {
+    // Create a buffer to capture the command's output
+    var outputBuffer bytes.Buffer
 
-	// Check for any execution error
-	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
-	}
+    // Set the output of the RootCmd to our buffer
+    RootCmd.SetOut(&outputBuffer)
 
-	// Check the output
-	expectedOutput := "Welcome to DLP's CLI!\n\n"
-	if stdout.String() != expectedOutput {
-		t.Errorf("Expected output: %q, but got: %q", expectedOutput, stdout.String())
-	}
+    // Create a TestingT adapter
+    tt := &testingTAdapter{t}
+
+    // Run the RootCmd
+    RootCmd.SetArgs([]string{"--help"}) // You can use any valid command line arguments
+    if err := RootCmd.Execute(); err != nil {
+        tt.Errorf("RootCmd execution error: %v", err)
+    }
+
+    // Check if the expected message is present in the output
+    output := outputBuffer.String()
+    expectedMessage := "Welcome to DLP's CLI!"
+    assert.Contains(tt, output, expectedMessage)
 }
