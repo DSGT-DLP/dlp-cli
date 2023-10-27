aa202e93b3e5110c624e4e3021f4c0775d70ab38
karkir0003 <karkir0003@gmail.com>
2023-10-26 22:00:39 -0400

    progress

--- cmd/backend/add/add_test.go
+++ cmd/backend/add/add_test.go
@@ -1,16 +1,47 @@
 package add
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
+    "bytes"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+    "github.com/spf13/cobra"
 )
 
-func TestSomething(t *testing.T) {
+type testingTAdapter struct {
+    t *testing.T
+}
 
-  // assert equality
-  assert.Equal(t, 123, 123, "they should be equal")
+func (ta *testingTAdapter) Errorf(format string, args ...interface{}) {
+    ta.t.Errorf(format, args...)
+}
 
-  // assert inequality
-  assert.NotEqual(t, 123, 456, "they should not be equal")
+func TestAddCmd(t *testing.T) {
+    // Create a buffer to capture the command's output
+    var outputBuffer bytes.Buffer
 
+    // Create a Cobra command to represent AddCmd
+    cmd := &cobra.Command{}
+
+    // Set the output of the AddCmd to our buffer
+    cmd.SetOut(&outputBuffer)
+
+    // Create a TestingT adapter
+    tt := &testingTAdapter{t}
+
+    // Create a new command for testing with the same flags
+    testCmd := &cobra.Command{}
+    testCmd.Flags().Bool("mamba", true, "Add package via mamba (used mainly if cross-platform compatibility is needed)")
+    testCmd.Flags().String("channel", "conda-forge", "Specify conda channel to install from (only used if --mamba flag is set)")
+    testCmd.Flags().String("env-name", "dlp", "Name of the environment")
+    testCmd.Flags().Bool("dev", true, "Add package as dev dependency")
+
+    //testCmd.setArgs("pip-install-test")
+    // Run the AddCmd with testCmd as the parent command
+    AddCmd.Run(testCmd, []string{"pip-install-test"})
+
+    // Check if the expected message is present in the output
+    output := outputBuffer.String()
+    expectedMessage := "IMPORTANT"
+    assert.Contains(tt, output, expectedMessage)
 }
