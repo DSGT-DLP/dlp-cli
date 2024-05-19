7b8ac6cbcb1aa4fe601d94c7e8e84d76cd3a74e0
andrewpeng02 <andrewpeng02@gmail.com>
2024-05-19 14:48:59 -0500

    update backend start to use docker

--- cmd/backend/start/start.go
+++ cmd/backend/start/start.go
@@ -4,8 +4,6 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package start
 
 import (
-	"fmt"
-
 	"github.com/DSGT-DLP/Deep-Learning-Playground/cli/cmd/backend"
 	"github.com/spf13/cobra"
 )
@@ -14,15 +12,13 @@ import (
 var StartCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Starts the training backend",
-	Long:  `Starts an instance of the training backend Django app in /training in the terminal`,
+	Long:  `Starts an instance of the training backend Django app in /training in the terminal. To change the backend port, set the environment var BACKEND_PORT. `,
 	Args:  cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		env_name := cmd.Flag("env-name").Value.String()
-		backend.ExecBashCmd("mamba", "run", "--live-stream", "-n", env_name, "poetry", "run", "python", "manage.py", "runserver", fmt.Sprintf("%v", cmd.Flag("port").Value))
+		backend.ExecBashCmd("docker", "compose", "up", "--build")
 	},
 }
 
 func init() {
 	backend.BackendCmd.AddCommand(StartCmd)
-	StartCmd.PersistentFlags().IntP("port", "p", 8000, "A port to run the backend on")
 }
